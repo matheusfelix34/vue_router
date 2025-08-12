be5ef0b76c38b0e95aeed9d299882fe7379149a8
matheusfelix34 <mfelix3411@gmail.com>
2025-08-11 21:22:14 -0300

    feat(src/views/UserView.vue):adiciona  pagina de usuários

---
+++ vue_router_app/src/views/UserView.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Usuários</h1>
+    <button @click="login()" >Logar</button>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router =useRouter();
+
+function login(){
+    console.log('logou');
+     router.push('/about')  ;
+  
+}
+</script>
