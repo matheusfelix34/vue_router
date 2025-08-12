d0ed959f48bca723b66649d706fe1bcf59208e4d
matheusfelix34 <mfelix3411@gmail.com>
2025-08-11 21:24:08 -0300

    feat(src/App.vue): adiciona rota de usuários ao menu superior de navegação

--- vue_router_app/src/App.vue
+++ vue_router_app/src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>|
-    <router-link :to="{name : 'servicos'}">Serviços</router-link>
+    <router-link :to="{name : 'servicos'}">Serviços</router-link>|
+    <router-link to="/usuarios/10">Usuários</router-link>
   </nav>
   <router-view/>
 </template>
