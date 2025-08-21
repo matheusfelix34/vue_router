1cbef550f3ac16495caab4f963478e6a227ed486
matheusfelix34 <mfelix3411@gmail.com>
2025-08-20 21:05:43 -0300

    style(src/App.vue): define background preto

--- vue_router_app/src/App.vue
+++ vue_router_app/src/App.vue
@@ -9,12 +9,17 @@
 </template>
 
 <style>
+
+html {
+    background-color: black;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: black;
 }
 
 nav {
