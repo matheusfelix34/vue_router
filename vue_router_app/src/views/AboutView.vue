cdfbce43dcc316673224d6a5e6c992b1f7caf83f
matheusfelix34 <mfelix3411@gmail.com>
2025-08-20 21:10:47 -0300

    feat(src/views/AboutView.vue): altera  contador via store

--- vue_router_app/src/views/AboutView.vue
+++ vue_router_app/src/views/AboutView.vue
@@ -2,4 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <br>
+  <div>
+    <p>Contador: {{ counter.count }}</p>
+    <p>Dobro: {{ counter.doubleCount }}</p>
+    <button @click="counter.increment">Aumentar</button>
+    <button @click="counter.reset">Zerar</button>
+
+    
+
+  </div>
 </template>
+
+<script setup>
+ import { useCounterStore } from '@/stores/useCounterStore'
+ const counter = useCounterStore()
+</script>
